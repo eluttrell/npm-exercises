f71f2055b282f5021edb37d46f38b7d73312c6e8
Elijah <elieel@gmail.com>
2016-12-06 10:22:14 -0500

    Complete lodash shuffle exercise

---
+++ practice.js
@@ -0,0 +1,13 @@
+const _ = require('lodash');
+
+
+// Ex1 - Lodash Shuffle
+//
+// function anagram(name) {
+//   var name = name.split('');
+//   var newName = _.shuffle(name).join('');
+//
+//   return newName;
+// }
+//
+// console.log(anagram(process.argv[2]));
